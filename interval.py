46edecca9615528500d8756827c097d6cd27aac8
coreymaaskant <coreymaaskant@gmail.com>
2026-01-22 14:07:29 -0500

    Added serial read in

--- interval.py
+++ interval.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 import threading
 import time
+import serial
+latest_data = {}
 
 app = Flask(__name__)
 
@@ -15,6 +17,23 @@ def intervalometer(start, end, interval):
         #print("Shutter triggered")
         # trigger_camera()
         time.sleep(interval)
+
+def read_serial(port="/dev/ttyUSB0", baud=57600):
+    global latest_data
+    ser = serial.Serial(port, baud, timeout=1)
+    while True:
+        line = ser.readline().decode().strip()
+        if line:
+            # Example line: "Temp:18.65 Humidity:27.68 Pressure:99493.50 TargetTemp:23°C ..."
+            parts = line.split()
+            data_dict = {}
+            for part in parts:
+                if ":" in part:
+                    key, value = part.split(":", 1)
+                    data_dict[key] = value
+            latest_data = data_dict
+
+threading.Thread(target=read_serial, daemon=True).start()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -39,8 +58,7 @@ def index():
             print("Manual shutter pressed")
             # trigger_camera()
 
-    return render_template("index.html")
-
+    return render_template("index.html", data=latest_data)
 @app.route("/stop")
 def stop():
     global intervalometer_running
