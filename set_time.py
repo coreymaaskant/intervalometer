7c73a1c2893e8464184d6715b8d1f9ab192935bd
coreymaaskant <45049965+coreymaaskant@users.noreply.github.com>
2026-03-10 19:52:35 -0400

    Changed so set time just sets the time and triggers a half shutter.

--- set_time.py
+++ set_time.py
@@ -2,38 +2,21 @@ import datetime
 import time
 import serial
 
-ser = serial.Serial('/dev/serial0', 9600, timeout=1)
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=1)
 time.sleep(1)
-start = "1300"
-end = "1400"
-timer = "00"
-alarm = "0"
-shutter = "0"
-temp = "20"
-halfshutter = "1"
+print("start time:")
+start = input()
+print("end time:")
+end = input()
+print("timer 00-99:")
+timer = input()
 dateSt=""
 x = datetime.datetime.now()
-dateSt = x.strftime('%y') + x.strftime('%m') + x.strftime('%d')+ x.strftime('%u') + x.strftime('%H') + x.strftime('%M') + x.strftime('%S') + timer + start + end + alarm + shutter + temp + halfshutter +  'x'
+dateSt = x.strftime('%y') + x.strftime('%m') + x.strftime('%d')+ x.strftime('%u') + x.strftime('%H') + x.strftime('%M') + x.strftime('%S') + timer + start + end + 'x'
 ser.write(dateSt.encode())
 print(x)
 print(dateSt)
-time.sleep(1)
 
-try:
-   ser.flush()
-   print(f"--- Listening on serial")
-
-   while True:
-      if ser.in_waiting > 0:
-         # Read the line and decode it from bytes to a string
-         line = ser.readline().decode('utf-8').rstrip()
-         print(f"Received: {line}")
-         time.sleep(0.01) # Small delay to prevent high CPU usage
-
-except KeyboardInterrupt:
-    print("\nStopping the reader...")
-finally:
-    if 'ser' in locals() and ser.is_open:
-        ser.close()
 ser.close()
 
